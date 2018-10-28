af9a1e034a1df4f177ae94007940755347305ceb
shyamkrishnanr28 <shyam.krishnan.r20@gmail.com>
2018-10-28 20:46:41 +0530

    Initial commit

---
+++ test_py_usb.py
@@ -0,0 +1,41 @@
+import usb.core
+import usb.util
+
+# find our device
+dev = usb.core.find(idVendor=0x04b4, idProduct=0x00f1)
+
+# was it found?
+if dev is None:
+    raise ValueError('Device not found')
+
+# set the active configuration. With no arguments, the first
+# configuration will be the active one
+dev.set_configuration()
+
+# get an endpoint instance
+cfg = dev.get_active_configuration()
+intf = cfg[(0,0)]
+
+ep_out = usb.util.find_descriptor(
+    intf,
+    # match the first OUT endpoint
+    custom_match = \
+    lambda e: \
+        usb.util.endpoint_direction(e.bEndpointAddress) == \
+        usb.util.ENDPOINT_OUT)
+		
+ep_in = usb.util.find_descriptor(
+    intf,
+    # match the first IN endpoint
+    custom_match = \
+    lambda e: \
+        usb.util.endpoint_direction(e.bEndpointAddress) == \
+        usb.util.ENDPOINT_IN)
+		
+print "\n", ep_in, "\n\n", ep_out
+
+assert ep_out is not None
+assert ep_in is not None
+
+# write the data
+ep_out.write('test')
