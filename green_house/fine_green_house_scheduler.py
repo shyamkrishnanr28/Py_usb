8fc1367c6b40a1f7632664e605226e2b001abc9c
shyamkrishnanr28 <shyam.krishnan.r20@gmail.com>
2019-05-08 00:13:24 +0530

    line spacing

--- green_house/fine_green_house_scheduler.py
+++ green_house/fine_green_house_scheduler.py
@@ -1,20 +1,15 @@
 
 #=============================== Macros ================================
-# 2 Hr interval 
 PUMP_ON_INTERVAL = 0.1 #60*60*2
-# For 1 Minute
 PUMP_ON_TIME = 0.5 #60*1
-
 
 #======================== Global variables =============================
 import sched, time
 import datetime
 from gpiozero import LED
 
-
 #======================== Global variables =============================
 SchedCnt=0
-
 
 # =========================== pump_on ==================================
 def pump_on(a='0'):
@@ -30,24 +25,22 @@ def pump_on(a='0'):
     file.write(str(datetime.datetime.now()))
     file.close()
 
-    # configure the stop time 
+    # configure the stop time
     s.enter(PUMP_ON_TIME, 1, pump_off, argument=('0'))
-
 
 # =========================== pump_off ==================================
 def pump_off(a='0'):
     # Turn off pump
     led.on()
     print("pump_off")
-    
+
     # configure the next on time
     s.enter(PUMP_ON_INTERVAL, 1, pump_on, argument=('0'))
-
 
 # =========================== main.py ==================================
 
 """ Read watering_log.txt and decide when to shcedule next pump_on()
-    This is needed to handle auto script restarting feature 
+    This is needed to handle auto script restarting feature
 
 try:
     file = open("watering_log.txt", "r")
